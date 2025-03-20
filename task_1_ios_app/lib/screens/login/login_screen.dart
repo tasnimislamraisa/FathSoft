460713d2ec204533cc3d8e663b3c19ab56f9bd22
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2025-03-20 16:46:12 +0600

    log in

---
+++ task_1_ios_app/lib/screens/login/login_screen.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:task_1_ios_app/screens/register/register_screen.dart';
+import 'package:task_1_ios_app/utility/app_color.dart';
+
+class LogInScreen extends StatefulWidget {
+  const LogInScreen({super.key});
+
+  @override
+  State<LogInScreen> createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+  final GlobalKey<FormState> _formState = GlobalKey<FormState>();
+  bool _signInApiProgress = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formState,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 160),
+                  Text(
+                    'Get Started With',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _emailTEController,
+                    decoration: InputDecoration(hintText: 'Email'),
+                    keyboardType: TextInputType.emailAddress,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return 'Enter Email Address';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _passwordTEController,
+                    decoration: InputDecoration(hintText: 'Password'),
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return 'Enter your password';
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 10),
+               ElevatedButton(
+                        onPressed: () {
+                          _onTapNextButton();
+                        },
+                        child: const Icon(
+                          Icons.arrow_forward_ios_outlined,
+                        ),
+                      ),
+                  const SizedBox(height: 10),
+                  Center(
+                    child: TextButton(
+                      onPressed: _onTapForgetPassword,
+                      child: Text(
+                        'Forget Password?',
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Don\'t have an account? ',
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(0.8),
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 0.4,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'Sign Up',
+                            style: TextStyle(color: AppColors.themeColor),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const Register(),
+                                    ));
+                              },
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+
+    );
+  }
+
+  void _onTapNextButton() {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const Register(),
+          ),
+        );
+
+  }
+
+  void _onTapForgetPassword() {
+    /*
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EmailVerificationScreen(),
+      ),
+    );
+    */
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _emailTEController.dispose();
+    _passwordTEController.dispose();
+    super.dispose();
+  }
+}
+
+
+
